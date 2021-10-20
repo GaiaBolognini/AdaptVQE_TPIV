276d5c2bca49dccebc9ca05fcae2390219461f9d
GaiaBolognini <gaiastella.bolognini@gmail.com>
2021-10-20 20:50:53 +0200

    Changed the operator pool, using double and single excitations already provided by pennylane

--- Programs/Adapt-VQE.py
+++ Programs/Adapt-VQE.py
@@ -4,7 +4,6 @@ import pennylane as qml
 from pennylane import numpy as np
 from pennylane import qchem
 import matplotlib.pyplot as plt
-import Operator_Pool as OP
 
 #definition of the moelcule and Hamiltonian
 symbols = ["H", "H"]
@@ -13,58 +12,79 @@ coordinates = np.array([0.0, 0.0, -distance/2, 0.0, 0.0, distance/2])
 a_electrons = 2
 a_orbitals = 2
 
-Hamiltonian, qubits = qml.qchem.molecular_hamiltonian(symbols, coordinates, active_electrons = a_electrons, active_orbitals = a_orbitals)
+Hamiltonian, qubits = qml.qchem.molecular_hamiltonian(symbols, coordinates)
+
+singles, doubles = qchem.excitations(a_electrons, qubits)
+excitation_operators = singles + doubles
+print('Excitations: ', excitation_operators)
+print('Total number of possible excitations: ', len(singles) + len(doubles))
+
 
 #global variables definition
 hf_state = qchem.hf_state(a_electrons, qubits)
 dev = qml.device("default.qubit", wires = qubits)
-opt = qml.GradientDescentOptimizer(stepsize=0.02)
+dev_draw = qml.device('qiskit.aer', wires=qubits)
+opt = qml.GradientDescentOptimizer(stepsize=0.5)
 
-threshold = 10**(-2)
 params_selected = []#parameters selected in the VQE agorithm
 arg_selected = []#operators from the operator pool selected (which argument)
-theta = -np.pi/4 #parameter chosen to evaluate the gradient
-shift = 0.05
+threshold = 10**(-4)
+theta = 0.
+s= 1/2
+shift = np.pi/(4*s)
 energy, phi = [], []
 
-#operator pool
-def apply (params, operator):
-    return operator(params)
+@qml.qnode(dev)
+def circuit(param, wires, excitation=0):
+    qml.BasisState(hf_state, wires=wires)
 
-operator_pool = [OP.operator_1, OP.operator_2, OP.operator_3, OP.operator_4, OP.operator_5]
+    for i in range(len(arg_selected)):
+       if len(excitation_operators[arg_selected[i]]) == 4:
+           qml.DoubleExcitation(params_selected[i], wires= excitation_operators[arg_selected[i]])
+       else:
+           qml.SingleExcitation(params_selected[i], wires=excitation_operators[arg_selected[i]])
 
-for i in range (60):
+    #adding the last operator
+    if (len(excitation) == 4):
+        qml.DoubleExcitation(param, wires=excitation)
+    else:
+        qml.SingleExcitation(param, wires=excitation)
+
+    return qml.expval(Hamiltonian)
+
+
+
+@qml.qnode(dev)
+def circuit_VQE(params, wires=range(qubits)):
+    qml.BasisState(hf_state, wires=wires)
+
+    for i in range(len(arg_selected)):
+        if len(excitation_operators[arg_selected[i]]) == 4:
+            qml.DoubleExcitation(params[i], wires=excitation_operators[arg_selected[i]])
+        else:
+            qml.SingleExcitation(params[i], wires=excitation_operators[arg_selected[i]])
+
+    return qml.expval(Hamiltonian)
+
+
+for i in range(30):
     print(i)
 
-    #circuit definition to choose the new operator, returning the expectation value of the Hamiltonian
-    @qml.qnode(dev)
-    def circuit(param, wires, number=0):
-        qml.BasisState(hf_state, wires=wires)
-        #adding the already selected operators
-        for j in range (len(arg_selected)):
-            apply(params_selected[j], operator_pool[arg_selected[j]])
-
-        #adding a different operator
-        apply(param, operator_pool[number])
-
-        return qml.expval(Hamiltonian)
-
-
-
-    #evaluating the gradient chianging the operator in the pool
+    #evaluating the gradient changing the operator in the pool
     circuit_gradients = []
-    for i in range(len(operator_pool)):
+    for i in range(len(excitation_operators)):
 
         #evaluating the gradient using the grad function
         #ansatz = lambda param : circuit(param, wires=range(qubits), number=i)
         #grad = qml.grad(ansatz)
         #circuit_gradients.append(grad(theta))
 
-        #evaluating the gradient using the parameter shift rule
-        grad = circuit(theta+shift, wires=range(qubits),number=i) - circuit(theta-shift, wires=range(qubits) , number=i)
+        # evaluating the gradient using the parameter shift rule
+        grad = s * (circuit(theta + shift, wires=range(qubits), excitation=excitation_operators[i]) - circuit(theta - shift, wires=range(qubits), excitation=excitation_operators[i]))
+        # print(grad)
         circuit_gradients.append(grad)
 
-    #print('Gradient: ', circuit_gradients)
+    print('Gradient: ', circuit_gradients)
 
     #taking tha max value of the gradient
     max_grad = max(np.abs(circuit_gradients))
@@ -77,20 +97,9 @@ for i in range (60):
     arg_selected.append(np.argmax(np.abs(circuit_gradients)))
 
 
-
-    #applying the VQE to find the best set of parameters:
-    @qml.qnode(dev)
-    def circuit_VQE(params, wires=range(qubits)):
-        qml.BasisState(hf_state, wires=wires)
-        # adding the chosen operators
-        for j in range(len(arg_selected)):
-            apply(params[j], operator_pool[arg_selected[j]])
-
-        return qml.expval(Hamiltonian)
-
-
-    convergence = 10 ** (-3)
-    phi.append(-np.pi/4)#parameter initialization
+    #applying the VQE to find the best set of parameters
+    convergence = 10 ** (-8)
+    phi.append(theta)#parameter initialization
 
     energy_old = circuit_VQE(phi)
     print('Initial energy: ', energy_old)
@@ -103,7 +112,7 @@ for i in range (60):
         phi = opt.step(circuit_VQE, phi)
         energy_new = circuit_VQE(phi)
 
-    #print('Best parameter', phi)
+    print('Best parameter', phi)
     print('Optimized energy: ', energy_new)
     params_selected = phi
     energy.append(energy_new)
@@ -115,8 +124,11 @@ for i in range (60):
 def circuit_final(wires=range(qubits)):
     qml.BasisState(hf_state, wires=wires)
     # adding the chosen operators
-    for j in range(len(arg_selected)):
-        apply(params_selected[j], operator_pool[arg_selected[j]])
+    for i in range(len(arg_selected)):
+       if len(excitation_operators[arg_selected[i]]) == 4:
+           qml.DoubleExcitation(params_selected[i], wires=excitation_operators[arg_selected[i]])
+       else:
+           qml.SingleExcitation(params_selected[i], wires=excitation_operators[arg_selected[i]])
 
     return qml.expval(Hamiltonian)
 
@@ -134,7 +146,7 @@ plt.figure()
 plt.title("Energy Optimization")
 plt.plot(energy, 'o-')
 plt.xlabel("Iterations")
-plt.ylabel("Energy, a.u.")
+plt.ylabel("Energy, Ha")
 plt.legend()
 plt.savefig('/mnt/c/Users/gaias/Desktop/Adapt_VQE_TPIV/Images/Energy_Adapt_VQE.pdf')
 plt.show()
